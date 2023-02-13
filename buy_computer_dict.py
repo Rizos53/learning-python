cc2fae9f3ee0ebc89987e5de986ab4a671f73eb5
Semih Kocaman | Routz <semih.kocaman@routz.nl>
2023-02-13 10:39:49 +0100

    Finalized the buy_computer_dict function

--- buy_computer_dict.py
+++ buy_computer_dict.py
@@ -8,18 +8,20 @@ available_parts = {"1": "computer",
 
 current_choice = None
 added_items = {}
+
 while current_choice != "0":
     if current_choice in available_parts:
         chosen_part = available_parts[current_choice]
+        added_items[current_choice] = available_parts[current_choice]
         print(f"adding {chosen_part}")
-        for added_items in current_choice:
-            current_choice += added_items
-            print(f"Added items: {added_items}")
 
     else:
         print("Please add options from the list: ")
         for key, value in available_parts.items():
             print(f"{key}: {value}")
         print("0: to finish")
+
+    print("Current items in cart: ")
+    print(added_items)
 
     current_choice = input("> ")
